a1839851e8026a86aa1be7c22b6b3df6fb2e376e
IVSoftware <tgregor@ivsoftware.com>
2020-05-28 12:59:57 -0600

    Added a demo project for "How do I make a hybrid RichTextBox-TabControl in Winforms?"

---
+++ hybrid-tabcontrol-richtextbox/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hybrid_tabcontrol_richtextbox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        protected override void OnHandleCreated(EventArgs e)
+        {
+            base.OnHandleCreated(e);
+            tabControl1.AddRTFPage();
+        }
+
+        private void buttonAddRtfTab_Click(object sender, EventArgs e)
+        {
+            tabControl1.AddRTFPage();
+        }
+    }
+}
---
+++ hybrid-tabcontrol-richtextbox/RTFTabControlExtension.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.TabControl;
+
+namespace hybrid_tabcontrol_richtextbox
+{
+    public static class Extensions
+    {
+        public static void AddRTFPage(this TabControl tabControl)
+        {
+            RichTextBox richTextBox = new RichTextBox()
+            {
+                Dock = DockStyle.Fill
+            };
+
+            TabPage rtfTabPage =
+                new TabPage("RTF" + (tabControl.TabCount + 1).ToString());
+
+            rtfTabPage.Controls.Add(richTextBox);
+            tabControl.TabPages.Add(rtfTabPage);
+        }
+    }
+}
